7e9f0548925b57ce23902d6a877c0352134103fc
Eduardo Escobar <eduardoescobar7@gmail.com>
2015-01-20 19:32:32 -0500

    Meti otro archivo

---
+++ Primer_programa.rb
@@ -0,0 +1,9 @@
+puts "Hola mundo"
+
+print "Por favor dime tu nombre"
+nombre=gets.chomp
+
+print "Genial! y cuantos años tienes?"
+edad=gets.chomp.to_i
+
+puts "Eres #{nombre} y tienes #{edad} años!"
